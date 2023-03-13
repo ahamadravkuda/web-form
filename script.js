a5d9044c9abbe3304ba8739d37ef30c0c4521662
ahamadravkuda <124757942+ahamadravkuda@users.noreply.github.com>
2023-03-13 17:09:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,120 +1,235 @@
-var selectedRow = null
+var form = `<div>
+  <div class="form-group">
+    <label for="name">Name</label>
+    <input type="text" class="form-control" id="name" aria-describedby="emailHelp" placeholder="Enter Your Name">
+  </div>
+  <div class="form-group mt-3">
+    <label for="email">Email</label>
+    <input type="email" class="form-control" id="email" placeholder="Enter Your email">
+  </div>
+  <div class="form-group mt-3">
+    <label for="number">Number</label>
+    <input type="number" class="form-control" id="number" placeholder="Enter Your number">
+  </div>
+  <div class="form-group mt-3">
+    <label for="brithday">Brithday</label>
+    <input type="date" class="form-control" id="date" placeholder="Enter Your brithday">
+  </div>
+  <div class="form-group mt-3">
+    <label for="time">appointment time</label>
+    <input type="time" class="form-control" id="time" placeholder="Enter Your time">
+  </div>
+  <div class="form-group mt-3">
+    <label for="age">Age</label>
+    <input type="range" class="form-control" id="age" placeholder="Enter Your age">
+  </div><br>
+  <div>
+  <div class="form-group mt-3">
+    <label for="color">color</label>
+    <input type="color" class="form-control" id="color">
+  </div><br>
+  <div>
+                    
+  <div>
+  <label>payment method</label>:--
+  <input type="radio" id="radio" name="fav_language" value="Phone pay">phone pay
+  <input type="radio" id="radio" name="fav_language" value="Phone pay"><span>Banking</span>
+  <input type="radio" id="radio" name="fav_language" value="Phone pay"><span>ATM</span>
+                    
+  </div><br>
+    <div class="form-group mt-3">
+    <label for="photo">photo</label>
+    <input type="file" class="form-control" id="file" placeholder="Enter Your photo">
+  </div>
+  <div>
+  <button type="submit" class="btn btn-primary mt-3" onclick="save()">Save</button>
+</div>`;
 
-function onFormSubmit() {
-    if (validate()) {
-        var formData = readFormData();
-        if (selectedRow == null)
-            insertNewRecord(formData);
-        else
-            updateRecord(formData);
-        resetForm();
-    }
-}
-
-function readFormData() {
-    var formData = {};
-    formData["fullName"] = document.getElementById("fullName").value;
-    formData["email"] = document.getElementById("email").value;
-    formData["number"] = document.getElementById("number").value;
-    formData["date"] = document.getElementById("date").value;
-    formData["time"] = document.getElementById("time").value;
-    formData["color"] = document.getElementById("color").value;
-    formData["radio"] = document.getElementById("Phone pay").value;
-    formData["file"] = document.getElementById("file").value;
-    
-    return formData;
-}
-
-function insertNewRecord(data) {
-    var table = document.getElementById("employeeList").getElementsByTagName('tbody')[0];
-    var newRow = table.insertRow(table.length);
-    cell1 = newRow.insertCell(0);
-    cell1.innerHTML = data.fullName;
-    cell2 = newRow.insertCell(1);
-    cell2.innerHTML = data.email;
-    cell3 = newRow.insertCell(2);
-    cell3.innerHTML = data.number;
-    cell4 = newRow.insertCell(3);
-    cell4.innerHTML = data.date;
-    cell5 = newRow.insertCell(4);
-    cell5.innerHTML = data.time;
-    cell6 = newRow.insertCell(5);
-    cell6.innerHTML = data.color;
-    cell7 = newRow.insertCell(6);
-    cell7.innerHTML = data.radio;
-    cell8 = newRow.insertCell(7);
-    cell8.innerHTML = data.file;
-    cell9= newRow.insertCell(8);
-    cell9.innerHTML = `<a onClick="onEdit(this)">Edit</a>
-                       <a onClick="onDelete(this)">Delete</a>`;
-}
-
-function resetForm() {
-    document.getElementById("fullName").value = "";
-    document.getElementById("email").value = "";
-    document.getElementById("number").value = "";
-    document.getElementById("date").value = "";
-    document.getElementById("time").value = "";
-    document.getElementById("color").value = "";
-    document.getElementById("Phone pay").value = "";
-    document.getElementById("file").value = "";
-    selectedRow = null;
-}
-
-function onEdit(td) {
-    selectedRow = td.parentElement.parentElement;
-    document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
-    document.getElementById("email").value = selectedRow.cells[1].innerHTML;
-    document.getElementById("number").value = selectedRow.cells[2].innerHTML;
-    document.getElementById("date").value = selectedRow.cells[3].innerHTML;
-    document.getElementById("time").value = selectedRow.cells[4].innerHTML;
-    document.getElementById("color").value = selectedRow.cells[5].innerHTML;
-    document.getElementById("radio").value = selectedRow.cells[5].innerHTML;
-    document.getElementById("file").value = selectedRow.cells[6].innerHTML;
-}
-function updateRecord(formData) {
-    selectedRow.cells[0].innerHTML = formData.fullName;
-    selectedRow.cells[1].innerHTML = formData.email;
-    selectedRow.cells[2].innerHTML = formData.number;
-    selectedRow.cells[3].innerHTML = formData.date;
-    selectedRow.cells[4].innerHTML = formData.time;
-    selectedRow.cells[5].innerHTML = formData.color;
-    selectedRow.cells[6].innerHTML = formData.radio;
-    selectedRow.cells[7].innerHTML = formData.file;
-}
-
-function onDelete(td) {
-    if (confirm('Are you sure to delete this record ?')) {
-        row = td.parentElement.parentElement;
-        document.getElementById("employeeList").deleteRow(row.rowIndex);
-        resetForm();
-    }
-}
-function validate() {
-    isValid = true;
-    if (document.getElementById("fullName").value == "") {
-        isValid = false;
-        document.getElementById("fullNameValidationError").classList.remove("hide");
+function table() {
+    let table = `<table class="table">
+  <thead>
+    <tr>
+      <th clsaa="col-1">NO</th>
+      <th clsaa="col-3">Name</th>
+      <th clsaa="col-4">Email</th>
+      <th clsaa="col-4">Number</th>
+      <th clsaa="col-4">Date</th>
+      <th clsaa="col-4">time</th>
+      <th clsaa="col-4">Age</th>
+      <th clsaa="col-4">color</th>
+      <th clsaa="col-4">payment method</th>
+      <th clsaa="col-4">photo</th>
+      <th clsaa="col-2">Edit</th>
+      <th clsaa="col-2">Delete</th>
+    </tr>
+  </thead>
+  <tbody>`;
+    for (let i = 0; i < details.length; i++){
+        table = table + `<tr>
+      <td>${i + 1}</td>
+      <td>${details[i].name}</td>
+      <td>${details[i].email}</td>
+      <td>${details[i].number}</td>
+      <td>${details[i].date}</td>
+      <td>${details[i].time}</td>
+      <td>${details[i].age}</td>
+      <td>${details[i].color}</td>
+      <td>${details[i].radio}</td>
+      <td>${details[i].file}</td>
+      <td><button type="button" class="btn btn-warning" onclick="edit(${i})">Edit</button></td>
+      <td><button type="button" class="btn btn-danger" onclick="deleteData(${i})">Delete</button></td>
+    </tr> `;
+    };
+    table = table+`</tbody>
+    </table>`;
+    document.getElementById("table").innerHTML = table;
+};
+document.getElementById("form").innerHTML = form;
+details = [];
+getData();
+table();
+function getData(){
+    let Data = localStorage.getItem("details");
+    if (Data) {
+        details = JSON.parse(Data);
     } else {
-        isValid = true;
-        if (!document.getElementById("fullNameValidationError").classList.contains("hide"))
-            document.getElementById("fullNameValidationError").classList.add("hide");
+        setData();
+    };
+};
+function setData() {
+    localStorage.setItem("details", JSON.stringify(details));
+};
+function save() {
+    let name = document.getElementById("name");
+    let email = document.getElementById("email");
+    let number = document.getElementById("number");
+    let date = document.getElementById("date");
+    let time = document.getElementById("time");
+    let age = document.getElementById("age");
+    let color = document.getElementById("color");
+    let file = document.getElementById("file");
+    let radio = document.getElementById("radio");
+
+
+    if (name.value == 0) {
+        alert("name is Empty");
+        return
     }
-    return isValid;
+    let data = {
+        name: name.value,
+        email: email.value,
+        number:number.value,
+        date:date.value,
+        time:time.value,
+        age:age.value,
+        color:color.value,
+        file:file.value,
+        radio:radio.value,
+    };
+    details.push(data);
+    setData();
+
+    // console.log(details)
+    // console.log(email.value)
+    table();
+    name.value = "";
+    email.value = "";
+    number.value="";
+    date.value="";
+    time.value="";
+    age.value="";
+    color.value="";
+    file.value="";
+    radio.value="";
+};
+function deleteData(index) {
+    details.splice(index, 1);
+    setData();
+    table();
+
+    // console.log('delete work')
+    // console.log(details)
+};
+
+function edit(index) {
+    let editForm = `<div>
+  <div class="form-group">
+    <label for="name">Update Name</label>
+    <input type="text" value="${details[index].name}" class="form-control" id="newName" aria-describedby="emailHelp" placeholder="Update Your Name">
+  </div>
+  <div class="form-group mt-3">
+    <label for="email">Email</label>
+    <input type="email" value="${details[index].email}" class="form-control" id="newEmail" placeholder="Update Your email">
+  </div>
+  <div class="form-group mt-3">
+    <label for="email">Number</label>
+    <input type="email" value="${details[index].number}" class="form-control" id="newNumber" placeholder="Update Your number">
+  </div>
+  <div class="form-group mt-3">
+    <label for="date">date</label>
+    <input type="date" value="${details[index].date}" class="form-control" id="newDate" placeholder="Update Your date">
+  </div>
+  </div>
+  <div class="form-group mt-3">
+    <label for="time">time</label>
+    <input type="time" value="${details[index].time}" class="form-control" id="newTime" placeholder="Update Your time">
+  </div>
+  <div class="form-group mt-3">
+  <label for="age">age</label>
+  <input type="age" value="${details[index].age}" class="form-control" id="newAge" placeholder="Update Your age">
+</div>
+<div class="form-group mt-3">
+  <label for="color">age</label>
+  <input type="color" value="${details[index].color}" class="form-control" id="newColor">
+</div>
+
+<div class="form-group mt-3">
+  <label for="method">payment method</label>
+  <input type="radio" value="${details[index].radio}" class="form-control" id="newMethod" placeholder="Update Your color">
+  <input type="radio" value="${details[index].radio}" class="form-control" id="newMethod" placeholder="Update Your color">
+  <input type="radio" value="${details[index].radio}" class="form-control" id="newMethod" placeholder="Update Your color">
+</div>
+  <label for="file">photo</label>
+  <input type="file" value="${details[index].file}" class="form-control" id="newFile" placeholder="Update Your photo">
+</div>
+   
+  <button type="submit" class="btn btn-primary mt-3" onclick="update(${index})">Update</button>
+</div>`;
+    document.getElementById("form").innerHTML = editForm;
+    // console.log('edit work');
+};
+function update(index) {
+    let newName = document.getElementById('newName');
+    let newEmail = document.getElementById('newEmail');
+    let newNumber = document.getElementById('newNumber');
+    let newDate = document.getElementById('newDate');
+    let newTime = document.getElementById('newTime');
+    let newAge = document.getElementById('newAge');
+    let newColor = document.getElementById('newColor');
+    let newFile = document.getElementById('newFile');
+    let newMethod = document.getElementById('newMethod');
+
+
+
+
+
+        details[index] = {
+        name: newName.value,
+        email: newEmail.value,
+        number:newNumber.value,
+        date:newDate.value,
+        time:newTime.value,
+        age:newAge.value,
+        color:newColor.value,
+        file:newFile.value,
+        radio:newMethod.value,
+        
+        
+    };
+    setData();
+    table();
+    document.getElementById("form").innerHTML = form;
+// console.log('update work')
+// console.log(details)
 }
 
-function manageData(){
-    //retrieving data
-    var name= document.getElementById("fName").value ;
-    var email=document.getElementById("Email").value;
-    var number=document.getElementById('Number').value;
-
-    //storing data
-
-    var num=localStorage.setItem("name",name);
-    var em=localStorage.setItem("email",email);
-    var num=localStorage.setItem("number",number);
-
-
-    
-}
